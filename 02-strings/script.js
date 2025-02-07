331bf0de53c13b4c3f4588884d9455fbe4c4c901
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-07 17:34:15 +0100

    edit es3 02-strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -24,10 +24,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = 'Jane' + ' ' +'Smith';
+const fullName = firstName + ' ' + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = 26;
+const numberOfLettersInAlphabet = null;
 console.log(numberOfLettersInAlphabet); // Output: 26
